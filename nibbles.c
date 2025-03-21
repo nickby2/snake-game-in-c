05da8d19b05c6baf3b78f127694af0185845be1e
Lucas Rodrigues Monteiro <180125974@aluno.unb.br>
2025-03-20 23:51:41 -0600

    snack: finish

--- nibbles.c
+++ nibbles.c
@@ -7,7 +7,12 @@
 #define HEIGHT 20
 #define SNAKE_MAX_LENGTH 100
 #define DEFAULT_WAIT 100
-
+#define WALL '*'
+#define FOOD '+'
+#define SNAKE '@'
+#define WALL_COLOR 1
+#define FOOD_COLOR 2
+#define SNAKE_COLOR 3
 typedef struct {
     int x[SNAKE_MAX_LENGTH]; 
     int y[SNAKE_MAX_LENGTH];
@@ -16,10 +21,36 @@ typedef struct {
     int directionY;
 } Snack;
 
+typedef struct {
+    int x;
+    int y;
+} Food;
+
+Food food;
 Snack snack;
 bool gameover = false;
-
 char telaBuffer[HEIGHT][WIDTH];
+int score = 0;
+
+void inicializer();
+void draw();
+void update();
+void processInput();
+void foodTruck();
+
+int main(){
+    inicializer();
+    while(!gameover){
+        draw();
+        update();
+        processInput();
+
+        napms(DEFAULT_WAIT);
+    }
+
+    endwin();
+
+}
 
 void inicializer(){
     initscr();
@@ -30,9 +61,9 @@ void inicializer(){
     curs_set(0);
     start_color();
     
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    init_pair(2, COLOR_RED, COLOR_BLACK);
-    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(SNAKE_COLOR, COLOR_GREEN, COLOR_BLACK);
+    init_pair(FOOD_COLOR, COLOR_RED, COLOR_BLACK);
+    init_pair(WALL_COLOR , COLOR_YELLOW, COLOR_BLACK);
 
     snack.comp = 1;
     snack.x[0] = WIDTH/2;
@@ -40,10 +71,12 @@ void inicializer(){
     snack.directionX = 1;
     snack.directionY = 0;
 
+    foodTruck();
+
     for(int i = 0; i < HEIGHT; i++){
         for(int j = 0; j < WIDTH; j++){
             if(i == 0 || i == HEIGHT-1 || j == 0 || j == WIDTH-1){
-                telaBuffer[i][j] = '#';
+                telaBuffer[i][j] = WALL;
         }
         else{
                 telaBuffer[i][j] = ' ';
@@ -52,20 +85,51 @@ void inicializer(){
     }
 }
 
+void foodTruck(){
+    food.x = 1 + rand() % (WIDTH - 2) + 1;
+    food.y = 1 + rand() % (HEIGHT - 2) + 1;
+}
+
 void draw(){
+
+    for(int i = 1; i < HEIGHT -1; i++){
+        for(int j = 1; j < WIDTH -1; j++){
+                telaBuffer[i][j] = ' ';
+        }
+    }
+
+    telaBuffer[food.y][food.x] = FOOD;
 
     for(int i = 0; i < snack.comp; i++){
         int x = snack.x[i];
         int y = snack.y[i]; 
-        telaBuffer[y][x] = '@';
+        telaBuffer[y][x] = SNAKE;
     }
 
     for(int i = 0; i< HEIGHT; i++){
         for(int j=0; j<WIDTH; j++){
-            mvaddch(i, j, telaBuffer[i][j]);
+            if(telaBuffer[i][j] == WALL){
+                attron(COLOR_PAIR(WALL_COLOR));
+                mvaddch(i, j, telaBuffer[i][j]);
+                attroff(COLOR_PAIR(WALL_COLOR));
+            }
+            else if(telaBuffer[i][j] == FOOD){
+                attron(COLOR_PAIR(FOOD_COLOR));
+                mvaddch(i, j, telaBuffer[i][j]);
+                attroff(COLOR_PAIR(FOOD_COLOR));
+            }
+            else if(telaBuffer[i][j] == SNAKE){
+                attron(COLOR_PAIR(SNAKE_COLOR));
+                mvaddch(i, j, telaBuffer[i][j]);
+                attroff(COLOR_PAIR(SNAKE_COLOR));
+            }
+            else{
+                mvaddch(i, j, telaBuffer[i][j]);
         }
     }
+}
 
+    mvprintw(HEIGHT, 0, "Score: %d", score);
     refresh();
 }
 
@@ -73,8 +137,33 @@ void draw(){
             int novoX = snack.x[0] + snack.directionX;
             int novoY = snack.y[0] + snack.directionY;
 
-            if(novoX <= 0 || novoX >= WIDTH-1 || novoY <= 0 || novoY >= HEIGHT-1){
-                gameover = true;
+            if(novoX < 0){
+                novoX = WIDTH - 2;
+            } else if(novoX >= WIDTH - 1){
+                novoX = 1;
+            } 
+
+            if(novoY < 0){
+                novoY = HEIGHT - 2;
+            } else if(novoY >= HEIGHT - 1){
+                novoY = 1;
+            }
+
+            if(novoX == food.x && novoY == food.y){
+                snack.comp++;
+                score += 10;
+                foodTruck();
+            }
+
+            for(int i = 0; i < snack.comp; i++){
+                if(novoX == snack.x[i] && novoY == snack.y[i]){
+                    gameover = true;
+                }
+            }
+
+            for(int i = snack.comp - 1; i > 0; i--){
+                snack.x[i] = snack.x[i-1];
+                snack.y[i] = snack.y[i-1];
             }
             
             snack.x[0] = novoX;
@@ -117,18 +206,3 @@ void draw(){
                 break;
         }
     }
-
-
-int main(){
-    inicializer();
-    while(!gameover){
-        draw();
-        update();
-        processInput();
-
-        napms(DEFAULT_WAIT);
-    }
-
-    endwin();
-
-}
