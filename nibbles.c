57fe47f15fd67648b8395435179e6ad9d29ae943
Lucas Rodrigues Monteiro <180125974@aluno.unb.br>
2025-03-20 22:36:28 -0600

    snack: move

--- nibbles.c
+++ nibbles.c
@@ -6,6 +6,7 @@
 #define WIDTH 40
 #define HEIGHT 20
 #define SNAKE_MAX_LENGTH 100
+#define DEFAULT_WAIT 100
 
 typedef struct {
     int x[SNAKE_MAX_LENGTH]; 
@@ -23,6 +24,7 @@ char telaBuffer[HEIGHT][WIDTH];
 void inicializer(){
     initscr();
     cbreak();
+    nodelay(stdscr, TRUE);
     noecho();
     keypad(stdscr, TRUE);
     curs_set(0);
@@ -51,6 +53,13 @@ void inicializer(){
 }
 
 void draw(){
+
+    for(int i = 0; i < snack.comp; i++){
+        int x = snack.x[i];
+        int y = snack.y[i]; 
+        telaBuffer[y][x] = '@';
+    }
+
     for(int i = 0; i< HEIGHT; i++){
         for(int j=0; j<WIDTH; j++){
             mvaddch(i, j, telaBuffer[i][j]);
@@ -60,13 +69,66 @@ void draw(){
     refresh();
 }
 
+    void update(){
+            int novoX = snack.x[0] + snack.directionX;
+            int novoY = snack.y[0] + snack.directionY;
+
+            if(novoX <= 0 || novoX >= WIDTH-1 || novoY <= 0 || novoY >= HEIGHT-1){
+                gameover = true;
+            }
+            
+            snack.x[0] = novoX;
+            snack.y[0] = novoY;
+    }
+
+    void processInput(){
+        int tecla = getch();
+
+        switch(tecla){
+            case KEY_UP:
+                if(snack.directionY == 0){
+                    snack.directionY = -1;
+                    snack.directionX = 0;
+                }
+                
+                break;
+            case KEY_DOWN:
+                if(snack.directionY == 0){
+                    snack.directionY = 1;
+                    snack.directionX = 0;
+                }
+                break;
+            case KEY_LEFT:
+                if(snack.directionX == 0){
+                    snack.directionY = 0;
+                    snack.directionX = -1;
+                }
+                break;
+            case KEY_RIGHT:
+                if(snack.directionX == 0){
+                    snack.directionY = 0;
+                    snack.directionX = 1;
+                }
+                break;
+
+            case 'x':
+            case 'q':
+                gameover = true;
+                break;
+        }
+    }
 
 
 int main(){
     inicializer();
-    draw();
     while(!gameover){
-        
+        draw();
+        update();
+        processInput();
+
+        napms(DEFAULT_WAIT);
     }
+
+    endwin();
 
 }
